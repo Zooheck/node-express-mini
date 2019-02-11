7ca1e759eb0f330069a042b1d8c2815764ea1183
Zooheck <davidscottflack@gmail.com>
2019-02-11 15:45:31 -0600

    Implement general get request and post request

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json({success: true, users})
+        })
+        .catch(err => {
+            res.status(500).json({
+                success: false,
+                error: "The users information could not be retrieved."
+            })
+        })
+});
+
+server.post('/api/users', (req, res) => {
+    const hobbit = req.body;
+
+    db
+        .insert(hobbit)
+        .then(hobbit => {
+            res.status(201).json({success: true, hobbit});
+        })
+        .catch( ( { code, message } )  => {
+            res.status(code).json({success: false, message})
+        })
+
+});
+
+
+
+server.listen('4000', () => {
+    console.log('Server listening on port 4000')
+})
